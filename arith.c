a23cbbd591d7cfeb8c421f5232be1bf0566f5df8
Sushravya achar <sushravyag292@gmail.com>
2025-11-10 11:24:27 +0530

    Add files via upload

---
+++ arith.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=4;
+    printf("Addition:%d\n",a+b);
+    printf("Subraction:%d\n",a-b);
+    printf("Multipilcation:%d\n",a*b);
+    printf("Division:%d\n",a/b);
+    printf("Modulus:%d",a%b);
+    return 0;
+}
---
+++ indecrement.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i=5;
+    printf("i++= %d\n",i++); //post increament
+    printf("++i =%d\n",++i); //pre increament
+    printf("i-- = %d\n",i--); //post decrement
+    printf("--i =%d",--i);  //pre decrement
+    return 0;
+}
