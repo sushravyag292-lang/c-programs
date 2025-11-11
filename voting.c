9a617da09d04834d8af7419cc63c9eeedfb2add1
Sushravya achar <sushravyag292@gmail.com>
2025-11-11 07:51:34 +0530

    Add files via upload

---
+++ voting.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int age;
+    printf("Enter ur age:");
+    scanf("%d",&age);
+    if (age>=18){
+        printf("You can vote");}
+    else 
+        printf("You cannot vote");
+    return 0;
+    }
