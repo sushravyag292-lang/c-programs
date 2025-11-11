5385dc65661d6a6b36bab503a96f0abc7e47efac
Sushravya achar <sushravyag292@gmail.com>
2025-11-11 08:28:22 +0530

    Add files via upload

---
+++ positive2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("Enter two integers:");
+    scanf("%d %d",&a,&b);
+    if (a>0){
+        if (b>0){
+            printf("Both numbers are positive");
+        
+    }
+}
+return 0;
+    
+}
