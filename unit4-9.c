d8ccb43a2a4e7c44bd5781f6dd48099d1fb00d19
Sushravya achar <sushravyag292@gmail.com>
2026-02-06 13:55:57 +0530

    Add files via upload

---
+++ unit4-9.c
@@ -0,0 +1,9 @@
+// Program: Enum Example - Weekdays
+#include <stdio.h>
+enum Week {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+int main() {
+    enum Week day;
+    day = Wednesday;
+    printf("Numeric value of Wednesday is: %d\n", day);
+    return 0;
+}
